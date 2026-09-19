bec4fd62a4eb4361176a1a22fe421034e61df6e1
Honore <amounankouadio@gmail.com>
2026-09-19 19:05:59 -0400

    Adding temporary changes to Github

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -13,32 +13,16 @@ async function connectToDatabase() {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
     // Task 1: Connect to MongoDB
-    async function connectToDatabase() {
-        if (dbInstance){
-            return dbInstance
-        };
-
-        const client = new MongoClient(url);
-
-        // Task 1: Connect to MongoDB
-        await client.connect();
-
-        // Task 2: Connect to database giftDB and store in variable dbInstance
-        dbInstance = client.db(dbName);
-
-        // Task 3: Return the database instance
-        return dbInstance;
-    }
-
+    await client.connect();
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+    // Task 3: Return the database instance
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
